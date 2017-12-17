449244181989cb69c840bbc3dbe0890100bea1b0
captainabhious <captainabhious@gmail.com>
2017-12-16 21:20:39 -0800

    + original assets (flags, icon)

---
+++ Orignal Assets/Flags/README.txt
@@ -0,0 +1,5 @@
+Flags
+=====
+
+Designer: Nordic Factory (http://www.nordicfactory.com)
+License: Free for commercial use (Include link to authors website)
--- Pictionaire/classificationController.swift
+++ Pictionaire/classificationController.swift
@@ -247,10 +247,14 @@ class classificationController: UIViewController, ARSCNViewDelegate, UIPickerVie
     
     @IBOutlet weak var selectButton: UIButton!
     
+    
+    
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
    
         // set the view's delegate
         sceneView.delegate = self
